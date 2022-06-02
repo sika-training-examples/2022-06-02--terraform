cc8ddc71508c7b94f1999f8907dcae7072e3342d
Ondrej Sika <ondrej@ondrejsika.com>
2022-06-02 15:10:59 +0200

    feat: Add VM from module from terraform registry

--- vms.tf
+++ vms.tf
@@ -17,3 +17,16 @@ output "ondrejsika-foo" {
     price_monthly = module.vm--ondrejsika-foo.digitalocean_droplet.price_monthly
   }
 }
+
+module "ondrejsika-do-droplet--hello" {
+  source       = "ondrejsika/ondrejsika-do-droplet/module"
+  version      = "1.2.0"
+  droplet_name = "hello"
+  record_name  = "hello"
+  tf_ssh_keys = [
+    digitalocean_ssh_key.default.id,
+  ]
+  user_data = null
+  vpc_uuid  = null
+  zone_id   = var.cloudflare_zone_id
+}
