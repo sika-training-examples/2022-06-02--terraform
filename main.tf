dc69724590bb256143df9b4f8222bea5eaa658af
Ondrej Sika <ondrej@ondrejsika.com>
2022-06-02 14:10:32 +0200

    feat: Add cloud init example

--- main.tf
+++ main.tf
@@ -33,9 +33,37 @@ resource "digitalocean_droplet" "example" {
   }
 }
 
+resource "digitalocean_droplet" "cloud-init" {
+  image  = local.DEBIAN
+  name   = "cloud-init"
+  region = "fra1"
+  size   = local.SIZE.SMALL
+  ssh_keys = [
+    digitalocean_ssh_key.default.id,
+    digitalocean_ssh_key.default-ed25519.id,
+    data.digitalocean_ssh_key.paveljirka.id,
+  ]
+  user_data = <<EOF
+#cloud-config
+ssh_pwauth: yes
+password: asdfasdf2020
+chpasswd:
+  expire: false
+runcmd:
+  - |
+    apt update
+    apt install -y curl sudo git
+    curl -fsSL https://ins.oxs.cz/slu-linux-amd64.sh | sudo sh
+EOF
+}
+
 output "ipv4" {
   value = [
     for vm in digitalocean_droplet.example :
     vm.ipv4_address
   ]
+}
+
+output "cloud-init-ip" {
+  value = digitalocean_droplet.cloud-init.ipv4_address
 }
