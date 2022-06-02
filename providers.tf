de827d9a9daeeff3d0dcf489126dc5b561159a36
Ondrej Sika <ondrej@ondrejsika.com>
2022-06-02 11:26:33 +0200

    refactor: Split main.tf

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+}
+
+variable "digitalocean_token" {}
+
+provider "digitalocean" {
+  token = var.digitalocean_token
+}
