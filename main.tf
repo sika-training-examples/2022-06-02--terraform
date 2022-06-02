167847d71e7df4b5bd4129a03022ab504492248a
Ondrej Sika <ondrej@ondrejsika.com>
2022-06-02 10:50:30 +0200

    feat: Add example VM with IP address in output

--- main.tf
+++ main.tf
@@ -21,3 +21,18 @@ resource "digitalocean_ssh_key" "default-ed25519" {
   name       = "ondrejsika-ed25519"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtI4BsjxWHmRB3EzyQDSX5idgyjD67XL4WmIjz+pcG6 ondrej@sika-mac"
 }
+
+resource "digitalocean_droplet" "example" {
+  image  = "debian-10-x64"
+  name   = "ondrejsika"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    digitalocean_ssh_key.default.id,
+    digitalocean_ssh_key.default-ed25519.id,
+  ]
+}
+
+output "ipv4" {
+  value = digitalocean_droplet.example.ipv4_address
+}
