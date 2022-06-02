37e8de0cbae3f0361b34885ef7ed2fd72d769a05
Ondrej Sika <ondrej@ondrejsika.com>
2022-06-02 14:27:42 +0200

    feat: Add Cloudflare provider

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -1,6 +1,13 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
+provider "registry.terraform.io/cloudflare/cloudflare" {
+  version = "3.16.0"
+  hashes = [
+    "h1:+iAlHilx2l44li9Hnj0Sje7nHVwBJgaVCViy/9MGx2Q=",
+  ]
+}
+
 provider "registry.terraform.io/digitalocean/digitalocean" {
   version = "2.20.0"
   hashes = [
--- providers.tf
+++ providers.tf
@@ -3,6 +3,9 @@ terraform {
     digitalocean = {
       source = "digitalocean/digitalocean"
     }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
   }
 }
 
@@ -10,4 +13,10 @@ variable "digitalocean_token" {}
 
 provider "digitalocean" {
   token = var.digitalocean_token
+}
+
+variable "cloudflare_api_token" {}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
